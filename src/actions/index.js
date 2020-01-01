94454fb2aaaf075a507c63d867c8e9c1e09f0e05
Yanay Zabary <yanay.tsabary@gmail.com>
2020-01-01 19:00:30 +0200

    initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,53 @@
+import streams from '../apis/streams';
+import history from '../history';
+import {
+    SIGN_IN,
+    SIGN_OUT,
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    DELETE_STREAM,
+    EDIT_STREAM
+} from './types';
+
+export const signIn = (userId) => {
+    return {
+        type: SIGN_IN,
+        payload: userId
+    };
+};
+
+export const signOut = () => {
+    return {
+        type: SIGN_OUT
+    };
+};
+
+export const createStream = formValues => async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post('/streams', { ...formValues, userId });
+    dispatch({ type: CREATE_STREAM, payload: response.data });
+    history.push('/');
+};
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = (recordId) => async dispatch => {
+    const response = await streams.get(`/streams/${recordId}`);
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (recordId, formValues) => async dispatch => {
+    const response = await streams.patch(`/streams/${recordId}`, formValues);
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+    history.push('/');
+};
+
+export const deleteStream = (recordId) => async dispatch => {
+    await streams.delete(`/streams/${recordId}`);
+    dispatch({ type: DELETE_STREAM, payload: recordId });
+    history.push('/');
+};
